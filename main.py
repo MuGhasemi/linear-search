0de92311bf674c893083726a18b10a94f7e15eec
Muhammad Ghasemi <mughasemi2008@gmail.com>
2022-12-08 21:56:57 +0330

    count equal number inputs

---
+++ main.py
@@ -0,0 +1,24 @@
+print("for exit of while, enter <exit> in input")
+list_search=[]
+while True:
+    text = input("Pls Enter : ")
+    if text == "exit":
+        break
+    elif text == " " or text == "":
+        continue
+    else:
+        list_search.append(text)
+
+number=0
+count=0
+c=0
+for i in range(len(list_search)):
+    for j in range(i,len(list_search)):
+        if list_search[j]==list_search[i]:
+            c+=1
+    if c>=count:
+        count=c
+        number=list_search[i]
+    c=0
+print(list_search)
+print(f"in list {count} number -> {number} exist ...")
